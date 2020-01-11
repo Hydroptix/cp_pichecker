1e0cb06e3e5dd5c547736d0c3fc91ae0d16d830f
Samuel Frazee <sfrazee@calpoly.edu>
2020-01-11 10:07:01 -0800

    Modified script to only require SSHing into the target once, getting both connection results and the load at the same time

--- checkpi.sh
+++ checkpi.sh
@@ -4,12 +4,13 @@
 #loop through all the Pis that should be available
 for HOST in pi01 pi02 pi03 pi04 pi05 pi06 pi07 pi08 pi09
 do	
-	#connect quietly and fail if it asks for a password or doesn't connect
-	ssh -q -o ConnectTimeout=5 -o BatchMode=yes -o PasswordAuthentication=no -o StrictHostKeyChecking=no $HOST.csc.calpoly.edu exit
-	if [ $? -eq 0 ]
+	#connect quietly and fail if it asks for a password or doesn't connect, if connection is successful get the load
+	LOAD=`ssh -q -o ConnectTimeout=5 -o BatchMode=yes -o PasswordAuthentication=no -o StrictHostKeyChecking=no $HOST.csc.calpoly.edu cat /proc/loadavg | awk '{print $1}'`
+	if [ -z "$LOAD" ]
 	then
-		#get the current load for this host
-		LOAD=`ssh -q -o ConnectTimeout=5 -o BatchMode=yes -o PasswordAuthentication=no -o StrictHostKeyChecking=no $HOST.csc.calpoly.edu cat /proc/loadavg | awk '{print $1}'`
+		echo $HOST down
+	else
+		#save this host if it has the lowest load
 		echo $HOST has load $LOAD
 		if [ -z "$BESTHOST" ]
 		then
@@ -22,13 +23,11 @@ do
 				BESTLOAD=$LOAD
 			fi
 		fi
-	else
-		echo $HOST down
 	fi
 done
 
 #Connect to the host with the lowest amount of load
-if [ -z  "$BESTHOST" ]
+if [ -z "$BESTHOST" ]
 then
 	echo no hosts up
 else
